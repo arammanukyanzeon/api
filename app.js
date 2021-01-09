621f06826cd11446daf1b992bce8734f1350e1dc
Aram <arammanukyanwork@gmail.com>
2021-01-09 20:09:28 +0400

    test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import router from './routes/routes.js'
+//import router from './routes/routes.js'
 
 const port = 8080;
 
@@ -18,9 +18,22 @@ class Server {
     config() {
         this.app.use(bodyParser.urlencoded({ extended: true }));
         this.app.use(bodyParser.json());
-        this.app.use(router)
+        //this.app.use(router)
         this.app.listen(port, () => {
             console.log(`App listening at http://localhost:${port}`)
+        })
+        this.app.get('/get', async (req, res) => {
+            try {
+                console.log("ALO")
+
+                if (post) {
+                    res.status(200).json(post)
+                } else {
+                    res.status(404).json({ message: "Post not found" })
+                }
+            } catch (err) {
+                res.status(500).json({ message: "Error getting posts", error: err })
+            }
         })
     }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,47 +1,47 @@
-import express from 'express';
-import { addTwitterClient, getAllTwiterClients } from "../../priotix-twitter/src/twitter_client/model/twitter_client.model.js";
-import { addTopics, getAllTopics } from "../../priotix-twitter/src/topics/model/topics.js";
+// import express from 'express';
+// import { addTwitterClient, getAllTwiterClients } from "../../priotix-twitter/src/twitter_client/model/twitter_client.model.js";
+// import { addTopics, getAllTopics } from "../../priotix-twitter/src/topics/model/topics.js";
 
-const router = express.Router();
+// const router = express.Router();
 
-// twitter
-router.post('/addtwitterclient', async (req, res) => {
-    await addTwitterClient(req.body);
-    res.send('client added')
-})
+// // twitter
+// router.post('/addtwitterclient', async (req, res) => {
+//     await addTwitterClient(req.body);
+//     res.send('client added')
+// })
 
-router.get('/getalltwitterclients', async (req, res) => {
-    try {
-        const post = await getAllTwiterClients();
+// router.get('/getalltwitterclients', async (req, res) => {
+//     try {
+//         const post = await getAllTwiterClients();
 
-        if (post) {
-            res.status(200).json(post)
-        } else {
-            res.status(404).json({ message: "Post not found" })
-        }
-    } catch (err) {
-        res.status(500).json({ message: "Error getting posts", error: err })
-    }
-})
+//         if (post) {
+//             res.status(200).json(post)
+//         } else {
+//             res.status(404).json({ message: "Post not found" })
+//         }
+//     } catch (err) {
+//         res.status(500).json({ message: "Error getting posts", error: err })
+//     }
+// })
 
-//topics
-router.post('/topics/add', async (req, res) => {
-    await addTopics(req.body);
-    res.send('client added')
-})
+// //topics
+// router.post('/topics/add', async (req, res) => {
+//     await addTopics(req.body);
+//     res.send('client added')
+// })
 
-router.get('/topics/getall', async (req, res) => {
-    try {
-        const post = await getAllTopics();
+// router.get('/topics/getall', async (req, res) => {
+//     try {
+//         const post = await getAllTopics();
 
-        if (post) {
-            res.status(200).json(post)
-        } else {
-            res.status(404).json({ message: "Post not found" })
-        }
-    } catch (err) {
-        res.status(500).json({ message: "Error getting posts", error: err })
-    }
-})
+//         if (post) {
+//             res.status(200).json(post)
+//         } else {
+//             res.status(404).json({ message: "Post not found" })
+//         }
+//     } catch (err) {
+//         res.status(500).json({ message: "Error getting posts", error: err })
+//     }
+// })
 
-export default router;
+// export default router;
